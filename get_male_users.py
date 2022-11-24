c8f6762df38f74970572c49dcd73c5173a646338
parkdin <abdukokhorovizatillo00@gmail.com>
2022-11-24 18:15:08 +0900

    done

--- get_male_users.py
+++ get_male_users.py
@@ -1,3 +1,4 @@
+from from_json import read_json
 def get_male_users(data:dict)->list:
     """Gets all male users from the data
     Args:
@@ -5,4 +6,11 @@ def get_male_users(data:dict)->list:
     Returns:
         list: A list of users
     """
-    pass
+    m_user = []
+    user = data["users"]
+    for i in user:
+        if i["gender"] == "male":
+            m_user.append(i)
+    return m_user
+data = read_json("users.json")
+print(get_male_users(data))
