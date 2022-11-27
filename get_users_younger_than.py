9bb16bdacdcc00d604efe1fcf624f09e03b264d9
parkdin <abdukokhorovizatillo00@gmail.com>
2022-11-27 14:22:09 +0900

    done

--- get_users_younger_than.py
+++ get_users_younger_than.py
@@ -1,3 +1,4 @@
+from from_json import read_json
 def get_users_younger_than(data:dict, age:int)->list:
     """Gets all users younger than a certain age from the data
     Args:
@@ -6,4 +7,12 @@ def get_users_younger_than(data:dict, age:int)->list:
     Returns:
         list: A list of users
     """
-    pass
+    user = data["users"]
+    y_users = []
+    for i in user:
+        if i["age"] < age:
+            y_users.append(i)
+    return y_users
+data = read_json("users.json")
+age = 24
+print(get_users_younger_than(data, age))
